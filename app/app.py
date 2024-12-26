5b625162385d05ab490ca8df754626a6fc85a092
Takato Oonishi <nnnkjktkt119@gmail.com>
2024-12-26 23:55:38 +0900

    add API end point by query parameter

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
+import pandas as pd
 import dash
 import dash_bootstrap_components as dbc
 from datareader import DataReader
 from pages.common import Config
+from flask import request, jsonify
 
 
 reader = DataReader()
@@ -21,6 +23,17 @@ app.layout = dbc.Container(
     ],
     fluid=True,
 )
+
+
+@app.server.route("/api")
+def download_data():
+    begin = request.args.get("begin", "20241201")
+    end = request.args.get("end", "20241201")
+    s_begin = pd.to_datetime(begin, format="%Y%m%d").strftime("%Y-%m-%d 00:00")
+    s_end = pd.to_datetime(end, format="%Y%m%d").strftime("%Y-%m-%d 23:59")
+    print(s_begin, s_end)
+    df = reader.read_spot_price(s_begin, s_end)
+    return jsonify(df[["date_time", "area_price_chubu"]].to_dict()) 
 
 
 if __name__ == "__main__":
