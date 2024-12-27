a0329f28db0ff8f7da19e7cf1aba8bdc3fbfc344
Takato Oonishi <nnnkjktkt119@gmail.com>
2024-12-27 21:58:25 +0900

    add system daemonスクリプト

---
+++ run_server.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+RUNPYTHON=/home/takato/uvpy312/bin/python
+SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
+
+cd $SCRIPT_DIR/app 
+$RUNPYTHON app.py --address  100.66.1.13 --port 8049  2>&1  > /dev/null
+
